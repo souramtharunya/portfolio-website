f0497eb679f85fd3356710afd3a9a4301bfb5340
souramtharunya <souramtharunya@gmail.com>
2026-06-07 15:46:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scrolling for navbar links
+
+document.querySelectorAll('nav a').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
